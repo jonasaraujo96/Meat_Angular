9c40a9240dfb1baa500e9f9f869d371b3b132cb4
Jonas <jonasaraujo96@gmail.com>
2018-10-17 17:50:14 -0300

    animações

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,14 +1,18 @@
-import { Restaurant } from './../restaurants/restaurant/restaurant.model';
-import { RestaurantsService } from './../restaurants/restaurants.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute} from '@angular/router'
+
+import {RestaurantsService} from '../restaurants/restaurants.service'
+
+import {Restaurant} from '../restaurants/restaurant/restaurant.model'
+
 @Component({
   selector: 'mt-restaurant-detail',
-  templateUrl: './restaurant-detail.component.html',
-  styleUrls: ['./restaurant-detail.component.css']
+  templateUrl: './restaurant-detail.component.html'
 })
 export class RestaurantDetailComponent implements OnInit {
+
   restaurant: Restaurant
+
   constructor(private restaurantsService: RestaurantsService,
               private route: ActivatedRoute) { }
 
--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,28 +1,49 @@
-import { MenuItem } from './../menu-item/menu-item.model';
-import { CartItem } from './cart-item.model';
+import {Injectable} from '@angular/core'
 
+import {CartItem} from './cart-item.model'
+import {MenuItem} from '../menu-item/menu-item.model'
+
+import {NotificationService} from '../../shared/messages/notification.service'
+
+@Injectable()
 export class ShoppingCartService {
-    items: CartItem [] = []
-    clear() {
-        this.items = [];
-    }
+  items: CartItem[] = []
 
-    addItem(item: MenuItem) {
-        let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id);
-        if (foundItem) {
-            foundItem.quantity = foundItem.quantity + 1;
-        } else {
-            this.items.push(new CartItem(item));
-        }
-    }
+  constructor(private notificationService: NotificationService){}
 
-    removeItem(item: CartItem) {
-        this.items.splice(this.items.indexOf(item), 1);
-    }
+  clear(){
+    this.items = []
+  }
 
-    total(): number {
-        return this.items
-            .map(item => item.value())
-            .reduce((prev, value) => prev + value, 0)
+  addItem(item:MenuItem){
+    let foundItem = this.items.find((mItem)=> mItem.menuItem.id === item.id)
+    if(foundItem){
+      this.increaseQty(foundItem)
+    }else{
+      this.items.push(new CartItem(item))
     }
+    this.notificationService.notify(`Você adicionou o item ${item.name}`)
+  }
+
+  increaseQty(item: CartItem){
+    item.quantity = item.quantity + 1
+  }
+
+  decreaseQty(item: CartItem){
+    item.quantity = item.quantity - 1
+    if (item.quantity === 0){
+      this.removeItem(item)
+    }
+  }
+
+  removeItem(item:CartItem){
+    this.items.splice(this.items.indexOf(item), 1)
+    this.notificationService.notify(`Você removeu o item ${item.menuItem.name}`)
+  }
+
+  total(): number{
+    return this.items
+      .map(item => item.value())
+      .reduce((prev, value)=> prev+value, 0)
+  }
 }
